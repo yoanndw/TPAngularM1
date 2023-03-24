4e1be9b35dd957d93c1fdea557aa1109e43ecd2c
Yoann DEWILDE <yoanndewilde@gmail.com>
2023-03-24 11:24:50 +0100

    Adapt `pokemonFilter` pipe to typing

--- src/app/search-input/pokemon-filter.pipe.ts
+++ src/app/search-input/pokemon-filter.pipe.ts
@@ -1,15 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Pokemon } from '../pokemon';
 
 @Pipe({
   name: 'pokemonFilter'
 })
 export class PokemonFilterPipe implements PipeTransform {
 
-  transform(value: any[], property: string, searchString?: string): any[] {
+  transform(value: Pokemon[], searchString?: string): Pokemon[] {
     if (typeof(value) !== "undefined") {
       if (typeof(searchString) !== "undefined" || searchString === "") {
         return value.filter(e => {
-          return e[property].toLowerCase().indexOf(searchString?.toLowerCase()) !== -1;
+          if (typeof e !== "undefined") {
+            return e.name!.toLowerCase().indexOf(searchString?.toLowerCase()) !== -1;
+          } else {
+            return false;
+          }
         });
       } else {
         return value;
